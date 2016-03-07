2ec713f76636e3d51f017afec1b5c8b0cfdae3c1
Timofey Asyrkin <tasyrkin@gmail.com>
2016-03-07 00:37:29 -0800

    Pattern matching on case classes

--- src/main/scala/com/tasyrkin/scalatest/Calculator.scala
+++ src/main/scala/com/tasyrkin/scalatest/Calculator.scala
@@ -1,14 +1,28 @@
 package com.tasyrkin.scalatest
 
-
-class Calculator[A](brand: String) {
+class CalculatorBase[A](brand: String) {
   def add(x: Int, y: Int) = x + y
 
   def f(x: A) = x.toString
 }
 
+case class Calculator(brand: String, model: String) extends CalculatorBase[Int](brand)
+
 object CalculatorTest extends App {
-  val calc = new Calculator[Int]("HP")
+  val calc = Calculator("HP", "20b")
   println(calc.add(10, 10))
   println(calc.f(10))
+  println(calc)
+
+  println(getType(calc))
+  println(getType(Calculator("Huawei", "_")))
+
+  def getType(calculator: Calculator): String = {
+    calculator match {
+      case Calculator("HP", "20b") => "scientific"
+      case Calculator("Samsung", "1a") => "financial"
+      case Calculator("Casio", "AB") => "simple"
+      case c@Calculator(brand, model) => s"$c is of unknown type"
+    }
+  }
 }
